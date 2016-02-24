ff746d4fa795f7e18438e6c093812ee126e90df8
zachkrasner <zachkrasner@gmail.com>
2016-02-24 11:28:13 -0800

    Added remove task functionality

--- todolist.rb
+++ todolist.rb
@@ -1,14 +1,22 @@
 class TodoList
   attr_accessor :title, :items
 
-  def initialize(list_title)
+  def initialize(list_title="My Todo List")
     @title = list_title
     @items = Array.new
   end
 
-  def add_new_item(description)
+  def rename_list(new_title)
+    @title = new_title
+  end
+
+  def add_item(description)
     new_item = Item.new(description)
     @items << new_item
+  end
+
+  def remove_item(item_position)
+    @items.delete_at(item_position - 1) ? "Item #{item_position} removed successfully!" : "Oops, couldn't find that item to remove";
   end
 end
 
