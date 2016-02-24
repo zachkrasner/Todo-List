e828fd07115c22afc01010ebaaed1980b0ceff21
zachkrasner <zachkrasner@gmail.com>
2016-02-24 11:14:27 -0800

    Added ability to Add New Item

--- todolist.rb
+++ todolist.rb
@@ -1,7 +1,24 @@
 class TodoList
-    # methods and stuff go here
+  attr_accessor :title, :items
+
+  def initialize(list_title)
+    @title = list_title
+    @items = Array.new
+  end
+
+  def add_new_item(description)
+    new_item = Item.new(description)
+    @items << new_item
+  end
 end
 
 class Item
-    # methods and stuff go here
+  attr_accessor :description, :completed_status
+
+  default_task = "New Task"
+
+  def initialize(description = default_task)
+    @description = description
+    @completed_status = false
+  end
 end
